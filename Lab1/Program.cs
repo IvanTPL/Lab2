895eb3c99d24a1a362d04da0e33c8ed2858b5061
Ivan TPL <ivan123050@gmail.com>
2021-11-16 16:53:56 +0300

    res

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -3,6 +3,7 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using System.IO;
 
 namespace Lab1
 {
@@ -31,8 +32,8 @@ namespace Lab1
 
     abstract class V3Data : IEnumerable<DataItem>
     {
-        public string str { get; set; }
-        public DateTime date { get; set; }
+        public string str { get; protected set; }
+        public DateTime date { get; protected set; }
         public V3Data(string str, DateTime date)
         {
             this.str = str;
@@ -133,15 +134,89 @@ namespace Lab1
         {
             return lst.GetEnumerator();
         }
+        public bool SaveBinary(string filename)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.OpenOrCreate);
+                BinaryWriter writer = new BinaryWriter(fs);
+                writer.Write(str);
+                writer.Write(date.ToString());
+                writer.Write(Count);
+                foreach (DataItem item in lst)
+                {
+                    writer.Write(item.x);
+                    writer.Write(item.y);
+                    writer.Write(item.vector.X);
+                    writer.Write(item.vector.Y);
+                }
+                writer.Close();
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return false;
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
+        static public bool LoadBinary(string filename, ref V3DataList v3)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.Open);
+                BinaryReader reader = new BinaryReader(fs);
+                string str = reader.ReadString();
+                DateTime date = Convert.ToDateTime(reader.ReadString());
+                int count = reader.ReadInt32();
+                if (v3 == null)
+                {
+                    v3 = new V3DataList(str, date);
+                }
+                else
+                {
+                    v3.str = str;
+                    v3.date = date;
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    double x = reader.ReadDouble();
+                    double y = reader.ReadDouble();
+                    float v_x = reader.ReadSingle();
+                    float v_y = reader.ReadSingle();
+                    Vector2 vct = new Vector2(v_x, v_y);
+                    DataItem dt = new DataItem(x, y, vct);
+                    v3.lst.Add(dt);
+                }
+                reader.Close();
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return false;
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
     }
 
     class V3DataArray : V3Data, IEnumerable<DataItem>
     {
-        public int n_x { get; }
-        public int n_y { get; }
-        public double step_x { get; }
-        public double step_y { get; }
-        public Vector2[,] arr { get; }
+        public int n_x { get; private set; }
+        public int n_y { get; private set; }
+        public double step_x { get; private set; }
+        public double step_y { get; private set; }
+        public Vector2[,] arr { get; private set; }
         public V3DataArray(string str, DateTime date) : base(str, date)
         {
             arr = new Vector2[0, 0];
@@ -224,18 +299,132 @@ namespace Lab1
         {
             return this.GetEnumerator();
         }
+        public bool SaveAsText(string filename)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.WriteLine(str);
+                sw.WriteLine(date.ToString());
+                sw.WriteLine(n_x.ToString());
+                sw.WriteLine(n_y.ToString());
+                sw.WriteLine(step_x.ToString());
+                sw.WriteLine(step_y.ToString());
+                for (int i = 0; i < n_x; i++)
+                {
+                    for (int j = 0; j < n_y; j++)
+                    {
+                        sw.WriteLine(arr[i, j].X.ToString());
+                        sw.WriteLine(arr[i, j].Y.ToString());
+                    }
+                }
+                sw.Close();
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return false;
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
+        static public bool LoadAsText(string filename, ref V3DataArray v3)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string str = sr.ReadLine();
+                DateTime date = Convert.ToDateTime(sr.ReadLine());
+                if (v3 == null)
+                {
+                    v3 = new V3DataArray(str, date);
+                }
+                else
+                {
+                    v3.str = str;
+                    v3.date = date;
+                }
+                v3.n_x = Convert.ToInt32(sr.ReadLine());
+                v3.n_y = Convert.ToInt32(sr.ReadLine());
+                v3.step_x = Convert.ToDouble(sr.ReadLine());
+                v3.step_y = Convert.ToDouble(sr.ReadLine());
+                v3.arr = new Vector2[v3.n_x, v3.n_y];
+                for (int i = 0; i < v3.n_x; i++)
+                {
+                    for (int j = 0; j < v3.n_y; j++)
+                    {
+                        float x = Convert.ToSingle(sr.ReadLine());
+                        float y = Convert.ToSingle(sr.ReadLine());
+                        v3.arr[i, j] = new Vector2(x, y);
+                    }
+                }
+                sr.Close();
+                return true;
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return false;
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+        }
     }
+        
+    
 
     class V3MainCollection
     {
-        protected DataItem? ret;
-        /*public DataItem? Ret
+        protected DataItem? d_max;
+        public DataItem? D_max
         {
             get 
-            { 
-
+            {
+                if (Count == 0)
+                    return null;
+                IEnumerable<DataItem> dQuery = lst.OrderByDescending(item => { if (item.Count == 0) { return 0; } return item.Max(x => Math.Sqrt(x.x * x.x + x.y * x.y)); }).First();
+                DataItem dt = dQuery.OrderByDescending(x => Math.Sqrt(x.x * x.x + x.y * x.y)).First();
+                return dt;
             }
-        }*/
+        }
+        protected IEnumerable<double> dup;
+        public IEnumerable<double> Dup
+        {
+            get
+            {
+                if (Count == 0)
+                    return null;
+                IEnumerable<DataItem> dQuery1 = lst.Aggregate<IEnumerable<DataItem>>((ls, x) => ls.Concat(x));
+                IEnumerable<DataItem> dQuery2 = dQuery1.Where(a => dQuery1.Count(b => b.x == a.x && b.y == a.y) > 1);
+                IEnumerable<double> res = (from item in dQuery2 select item.x).Distinct();
+                if (res.Count() == 0)
+                    return null;
+                return res;
+            }
+        }
+        protected IEnumerable<V3Data> date_min;
+        public IEnumerable<V3Data> Date_min
+        {
+            get
+            {
+                if (Count == 0)
+                    return null;
+                DateTime d_m = lst.Min(x => x.date);
+                IEnumerable<V3Data> dQuery = lst.Where(x => x.date == d_m);
+                return dQuery;
+            }
+        }
         private List<V3Data> lst;
         protected int count;
         public int Count
@@ -303,38 +492,78 @@ namespace Lab1
     
     class Program
     {
+        public static void Dbg1()
+        {
+            V3DataArray a = new V3DataArray("ID_1", DateTime.Now, 0, 0, 1, 1, Methods.F1);
+            V3DataArray b = new V3DataArray("ID_2", DateTime.Now);
+            a.SaveAsText("test.txt");
+            Console.WriteLine(a.ToLongString("F3"));
+            Console.WriteLine("");
+            V3DataArray.LoadAsText("test.txt", ref b);
+            Console.WriteLine(b.ToLongString("F3"));
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            V3DataList c = new V3DataList("ID_3", DateTime.Now);
+            c.AddDefaults(1, Methods.F2);
+            V3DataList d = new V3DataList("ID_4", DateTime.Now);
+            c.SaveBinary("test");
+            Console.WriteLine(c.ToLongString("F3"));
+            Console.WriteLine("");
+            V3DataList.LoadBinary("test", ref d);
+            Console.WriteLine(d.ToLongString("F3"));
+        }
+
+        public static void Dbg2()
+        {
+            V3MainCollection clct = new V3MainCollection();
+            V3DataList c1 = new V3DataList("ID_3", DateTime.Now);
+            V3DataList c2 = new V3DataList("ID_4", DateTime.UtcNow);
+            V3DataList c3 = new V3DataList("ID_5", DateTime.Now);
+            DataItem dt1 = new DataItem(0.0, 1.0, Methods.F2(0.0, 1.0));
+            DataItem dt2 = new DataItem(0.0, 1.0, Methods.F2(0.0, 2.0));
+            DataItem dt3 = new DataItem(1.0, 1.0, Methods.F2(1.0, 1.0));
+            c1.Add(dt1);
+            c2.Add(dt2);
+            c2.Add(dt3);
+            clct.Add(c1);
+            clct.Add(c2);
+            clct.Add(c3);
+            V3DataArray a = new V3DataArray("ID_1", DateTime.Now, 0, 0, 1, 1, Methods.F1);
+            clct.Add(a);
+            Console.WriteLine(clct.ToLongString("F3"));
+            Console.WriteLine("");
+
+            if (clct.D_max == null)
+                Console.WriteLine("Коллекция пустая.");
+            else
+                Console.WriteLine($"Элемент с максимальным расстоянием от точки, в которой измерено поле, до начала координат: {clct.D_max}");
+            Console.WriteLine("Элементы с минимальным значением даты измерения:");
+            if (clct.Date_min == null)
+                Console.WriteLine("Коллекция пустая.");
+            else
+            {
+                foreach (V3Data dat in clct.Date_min)
+                {
+                    Console.WriteLine(dat);
+                }
+            }
+            Console.WriteLine("Координаты x, которые встречаются более одного раза:");
+            if (clct.Dup == null)
+                Console.WriteLine("Нет элементов с повторяющимися координатами x.");
+            else
+            {
+                foreach (double dat in clct.Dup)
+                {
+                    Console.WriteLine(dat);
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            V3DataArray a = new V3DataArray("ID_1", DateTime.Now, 2, 2, 2.5, 2.5, Methods.F1);
-            Console.WriteLine(a.ToLongString("F3"));
-            V3DataList a_lst = (V3DataList)a;
-            Console.WriteLine(a_lst.ToLongString("F3"));
-            Console.WriteLine($"Array: count = {a.Count}; maxdistance = {a.MaxDistance}");
-            Console.WriteLine($"List from array: count = {a_lst.Count}; maxdistance = {a_lst.MaxDistance}");
-
+            Program.Dbg1();
             Console.WriteLine("");
-            V3DataArray b = new V3DataArray("ID_2", DateTime.Now, 2, 2, 4.5, 4.5, Methods.F1);
-            V3DataList c = new V3DataList("ID_3", DateTime.Now);
-            V3DataList d = new V3DataList("ID_4", DateTime.Now);
-            c.AddDefaults(2, Methods.F2);
-            d.AddDefaults(2, Methods.F3);
-            V3MainCollection clct = new V3MainCollection();
-            clct.Add(a);
-            clct.Add(b);
-            clct.Add(c);
-            clct.Add(d);
-            Console.WriteLine(clct.ToLongString("F3"));
-
-            Console.WriteLine("");
-            for (int i = 0; i < clct.Count; i++)
-            {
-                Console.WriteLine($"Collection #{i + 1}: count = {clct[i].Count}; maxdistance = {clct[i].MaxDistance}");
-            }
-            /*foreach(DataItem dat in a)
-            {
-                Console.WriteLine(dat);
-            }*/
-
+            Program.Dbg2();
         }
     }
 }
